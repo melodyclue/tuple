b2be6b7b46733d79e9709a5abf75d8bfe5d60f54
melodyclue <melody.router@gmail.com>
2024-12-12 23:31:22 +0900

    feat: add user dropdown

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,17 +1,25 @@
 import { signInWithOAuthAction, signOutAction } from '@/app/actions';
 import { hasEnvVars } from '@/utils/supabase/check-env-vars';
-import Link from 'next/link';
 import { Badge } from './ui/badge';
-import { Button } from './ui/button';
 import { createClient } from '@/utils/supabase/server';
 import { SubmitButton } from './submit-button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { getProfile } from '@/utils/getProfile';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default async function AuthButton() {
   const supabase = await createClient();
 
   const {
     data: { user },
-    error,
   } = await supabase.auth.getUser();
 
   if (!hasEnvVars) {
@@ -19,43 +27,17 @@ export default async function AuthButton() {
       <>
         <div className="flex items-center gap-4">
           <div>
-            <Badge variant={'default'} className="pointer-events-none font-normal">
+            <Badge variant="default" className="pointer-events-none font-normal">
               Please update .env.local file with anon key and url
             </Badge>
-          </div>
-          <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={'outline'}
-              disabled
-              className="pointer-events-none cursor-none opacity-75"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={'default'}
-              disabled
-              className="pointer-events-none cursor-none opacity-75"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
           </div>
         </div>
       </>
     );
   }
+
   return user ? (
-    <div className="flex items-center gap-4">
-      Logged in as {user.email}
-      <form action={signOutAction}>
-        <Button type="submit" variant={'outline'}>
-          Sign out
-        </Button>
-      </form>
-    </div>
+    <UserDropdown userId={user.id} />
   ) : (
     <div className="flex gap-2">
       <form>
@@ -65,20 +47,44 @@ export default async function AuthButton() {
           Sign in with Google
         </SubmitButton>
       </form>
-      {/* <Button asChild size="sm" variant={'outline'}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={'default'}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button> */}
     </div>
   );
 }
 
-// export default () => {
-//   return (
-//     <Suspense fallback={<div>Loading...</div>}>
-//       <AuthButton />
-//     </Suspense>
-//   );
-// };
+const UserDropdown = async ({ userId }: { userId: string }) => {
+  const profile = await getProfile(userId);
+
+  if (!profile) redirect('/onboard');
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer border">
+          <AvatarImage src={profile.imageUrl ?? undefined} />
+          <AvatarFallback>{profile.name.charAt(0)}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="z-10 mr-2 w-48 rounded-xl bg-white p-2">
+        <DropdownMenuItem asChild>
+          <Link href={`/${profile.username}`} className="block cursor-pointer px-4 py-2">
+            <div className="mb-0.5 text-[16px] font-semibold">{profile.name}</div>
+            {/* <div className="text-[12px] text-slate-500">{profile.headline}</div> */}
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="cursor-pointer py-2 text-[14px] transition-all hover:bg-slate-50">
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="cursor-pointer text-[14px] transition-all hover:bg-slate-50">
+          <form action={signOutAction}>
+            <button type="submit" className="text-rose-600">
+              Logout
+            </button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
