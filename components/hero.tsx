50e05801a02e3d32db67a91a7383dd6fb519f17b
melodyclue <melody.router@gmail.com>
2024-12-13 22:10:18 +0900

    feat: enhance hero component to display user profile link and button text based on profile existence

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,7 @@ import { GoogleSignIn } from '@/app/google-signin';
 import { createClient } from '@/utils/supabase/server';
 import Link from 'next/link';
 import { Button } from './ui/button';
+import { getProfile } from '@/utils/getProfile';
 
 export default async function Hero() {
   const supabase = await createClient();
@@ -10,16 +11,17 @@ export default async function Hero() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const profile = user ? await getProfile(user.id) : undefined;
+
   return (
     <div className="flex flex-col items-center gap-3">
       <h1 className="text-3xl font-bold">Put your Profile</h1>
       <p className="mx-auto max-w-xl text-zinc-600">The fastest way to build your profile</p>
-      {/* <div className="my-8 w-full bg-gradient-to-r from-transparent via-foreground/10 to-transparent p-[1px]" /> */}
       <div className="mt-4 flex flex-col gap-2">
         {user ? (
-          <Link href="/settings">
+          <Link href={profile ? `/${profile.username}` : '/onboard'}>
             <Button className="rounded-3xl border bg-white px-8 py-2 font-medium text-zinc-600 shadow-none">
-              Settings
+              {profile ? 'Edit Profile' : 'Create Profile'}
             </Button>
           </Link>
         ) : (
